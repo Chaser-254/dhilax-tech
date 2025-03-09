3a30dcbb4a8b4a9fff45b89bffc9e6547a856668
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-09 11:24:36 +0300

    deploying to netlify

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/navbar";
 import Home from "./pages/Home";
+import Navbar from "./components/navbar";
 import Footer from "./pages/footer";
 import Services from "./pages/Services";
 import Booking from "./pages/Booking";
