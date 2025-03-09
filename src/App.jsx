ffa6b2005f15e4e1ea414b09396ed17c116529a8
Chaser-254 <esibitaremmanuel316@gmail.com>
2025-03-09 11:45:18 +0300

    Fix dependency issue

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
-import Footer from "./pages/footer";
+import Footer from "./pages/Footer";
 import Services from "./pages/Services";
 import Booking from "./pages/Booking";
 import Contact from "./pages/Contact";
